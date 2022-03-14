437d5d96a40fc2559952f52d229e513644bb023c
irvanmnwr <irvanmnwr@gmail.com>
2022-03-14 09:26:09 +0700

    perubahan

---
+++ javascript_Introduction/Task/task1.js
@@ -0,0 +1,45 @@
+//Polindrom
+// const input = "qwq"
+// let teks = input.toUpperCase();
+// let hasil = ""
+// let x = teks.length-1
+
+// if(x>1){
+//   while(x>=0){
+//     hasil = hasil + teks[x]
+//     x = x - 1}
+  
+//   if(hasil == teks){
+//     console.log("Polindrom")
+//   }
+//   else{
+//     console.log("Tidak Polindrom")
+//   }
+// }
+// else{
+//   console.log("Inputan Harus lebih dari 2 huruf")
+// }
+
+
+// reverse word
+// const teks = "Saya irvan"
+// let hasil = " "
+// let temp = ""
+// let kata = 1
+// let x = teks.length-1
+
+// while(x>=0){
+//   if(teks[x]==" "){
+//   temp = temp + hasil
+//   hasil = ""
+//   kata = kata + 1
+//   }
+//   hasil = teks[x] + hasil
+//   x = x - 1
+// }
+// temp = temp+hasil
+// if(kata>1){
+// console.log(temp)
+// }else{
+//   console.log("Jumlah kata Harus Lebih Dari Satu")
+// }
