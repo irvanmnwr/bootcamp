1aac149266637d989d45f6c4a6663b2e196f0b86
irvanmnwr <irvanmnwr@gmail.com>
2022-03-18 06:44:00 +0700

    update materi js intro  part1

--- javascript_Introduction/Part_1/Destructing.js
+++ javascript_Introduction/Part_1/Destructing.js
@@ -1,11 +1,16 @@
-// const profile = {
-//     name: "Bagus",
-//     sister: {
-//         sister1: "Anna",
-//         sister2: "Bella"
-//     }
-// }
+const profile = {
+    name: "Bagus",
+    sister: {
+        sister1: "Anna",
+        sister2: "Bella"
+    },
+    name: "Bag",
+    sister: {
+        sister1: "An",
+        sister2: "Bel"
+    }
+}
 
-// const {sister2, sister1: saudaraPerempuan1} = profile.sister
-// console.log(saudaraPerempuan1)
-// console.log(sister2)
+const {sister2, sister1: saudaraPerempuan1} = profile.sister
+console.log(saudaraPerempuan1)
+console.log(sister2)
